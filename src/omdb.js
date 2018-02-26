fa1e80cfbe53bed908bb110087b62dead966c154
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-02-26 16:40:50 -0600

    YouTube Links

--- src/omdb.js
+++ src/omdb.js
@@ -12,10 +12,14 @@ const getMovies = (title) => {
       document.getElementById('poster-img').innerHTML += `<div id='results${i}'><img src="${results.Search[i].Poster}"></div><p>${results.Search[i].Title} ${results.Search[i].Year}</p></div>`;
 
       const apiKey = 'AIzaSyD50XUXFl0OiIviqMZTB5qIqzCJY9utQj4'; // pls don't steal me :)
-      fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${results.Search[i].Title}&maxResults=1&part=snippet`).then(response => response.json()
+      fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=${results.Search[i].Title}+${results.Search[i].Year}+Trailer&maxResults=1&part=snippet`).then(response => response.json()
         .then(results => {
-          let img = document.getElementById('results' + i);
-          img.setAttribute('data-attr', results.items[0].id.videoId);
+          let img = document.getElementById('results' + i).children[0];
+          // img.setAttribute('data-attr', results.items[0].id.videoId);
+            img.style.cursor = 'pointer';
+            img.addEventListener('click', () => {
+              window.open(`http://youtube.com/watch?v=${results.items[0].id.videoId}`, '_blank');
+            });
           console.log(results.items[0].id.videoId);
         })
         );
